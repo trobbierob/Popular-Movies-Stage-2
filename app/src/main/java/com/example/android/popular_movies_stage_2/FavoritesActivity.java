1b7d79b138b20e94c605b90e140fd011747195f4
trobbierob <troberts@my.hpu.edu>
2018-06-25 18:40:25 -0400

    Display data in simple listview

--- app/src/main/java/com/example/android/popular_movies_stage_2/FavoritesActivity.java
+++ app/src/main/java/com/example/android/popular_movies_stage_2/FavoritesActivity.java
@@ -2,41 +2,46 @@ package com.example.android.popular_movies_stage_2;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
-import android.widget.TextView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 import com.example.android.popular_movies_stage_2.sample.SampleDataProvider;
 
+import java.util.ArrayList;
 import java.util.Collections;
-import java.util.Comparator;
 import java.util.List;
 
 public class FavoritesActivity extends AppCompatActivity {
 
-    TextView tv;
+    //TextView tv;
     List<Movie> movieList = SampleDataProvider.movieList;
+    List<String> movieTitles = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_favorites);
 
-        //Movie movie = new Movie("blank", "blank");
+        //tv = findViewById(R.id.tv);
+        //tv.setText("");
 
-        tv = findViewById(R.id.tv);
-
-        tv.setText("");
-
-        Collections.sort(movieList, new Comparator<Movie>() {
+        /*Collections.sort(movieList, new Comparator<Movie>() {
             @Override
             public int compare(Movie movie, Movie t1) {
                 return movie.getTitle().compareTo(t1.getTitle());
             }
-        });
+        });*/
 
         for (Movie movie : movieList) {
-            tv.append(movie.getTitle() + "\n");
-            Log.i("TAG", "Movie is: " + movie.getTitle());
+            //tv.append(movie.getTitle() + "\n");
+            movieTitles.add(movie.getTitle());
         }
+        Collections.sort(movieTitles);
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(
+                this,android.R.layout.simple_list_item_1, movieTitles);
+
+        ListView listView = findViewById(R.id.fav_listView);
+        listView.setAdapter(adapter);
     }
 }
--- app/src/main/java/com/example/android/popular_movies_stage_2/sample/SampleDataProvider.java
+++ app/src/main/java/com/example/android/popular_movies_stage_2/sample/SampleDataProvider.java
@@ -21,6 +21,24 @@ public class SampleDataProvider {
         addItem(new Movie("Lady Deadpool", "4"));
         addItem(new Movie("Dogpool", "5"));
         addItem(new Movie("Cable & Deadpool", "6"));
+        addItem(new Movie("Deadpool 2", "7"));
+        addItem(new Movie("Wheelpool", "8"));
+        addItem(new Movie("Dealpool", "9"));
+        addItem(new Movie("Healpool", "10"));
+        addItem(new Movie("Peelpool", "11"));
+        addItem(new Movie("Bedpool", "12"));
+        addItem(new Movie("Pool", "13"));
+        addItem(new Movie("Dead", "14"));
+        addItem(new Movie("Mealpool", "15"));
+        addItem(new Movie("Mr. Lady Deadpool", "16"));
+        addItem(new Movie("Lady Dogpool", "17"));
+        addItem(new Movie("Cable & Deadpool 2", "18"));
+        addItem(new Movie("Whackpool 2", "19"));
+        addItem(new Movie("Wackpool", "20"));
+        addItem(new Movie("Black Deadpool", "21"));
+        addItem(new Movie("Blackpool", "22"));
+        addItem(new Movie("Orangepool", "23"));
+        addItem(new Movie("Beetlepool", "24"));
     }
 
     private static void addItem(Movie movie) {
